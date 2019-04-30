01642367dd442cbf46b6a0f156fd2cfc32c8688a
Chris Moore - C33997 <Chris.Moore@microchip.com>
2019-04-29 17:33:39 -0700

    Set correct colors for 1861 companies Added class to handle the auction of minor companies in the start round

---
+++ src/main/java/net/sf/rails/game/specific/_1861/StartRoundMinors_1861.java
@@ -0,0 +1,22 @@
+package net.sf.rails.game.specific._1861;
+
+import java.util.SortedSet;
+
+import net.sf.rails.game.GameManager;
+import net.sf.rails.game.StartRound_AuctionOnly;
+
+public class StartRoundMinors_1861 extends StartRound_AuctionOnly {
+
+	protected StartRoundMinors_1861(GameManager parent, String id) {
+		super(parent, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected SortedSet<String> getStartSpaces(int i) {
+		// TODO Auto-generated method stub
+
+		return null;
+	}
+
+}
